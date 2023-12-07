ad27b6798dab1563c86cd1a861921a14d6115021
Monkiiif <roussafimoncif0@gmail.com>
2023-12-06 17:41:11 -0800

    MOK

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
@@ -13,4 +12,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
